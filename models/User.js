e985ade49f0d5ba7c3a344cf57a292207022d756
kyuyoungoh <oqoq59@gmail.com>
2021-10-11 22:47:13 +0900

    add jsonwebtoken

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -46,10 +47,30 @@ userSchema.pre('save', function (next) {
                 next()
             });
         });
+    } else {
+        next()
     }
-
-    
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword <-> bcrypt
+    bcrypt.compare(plainPassword, this.password, function(err,isMatch){
+        if (err) return cb(err),
+            cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    //jsonwebtoken -> 토큰 생성
+    var token = jwt.sign(user._id.toHexString(), "secretToken")
+
+    user.token = token
+    user.save(function(err, user) {
+        if (err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
