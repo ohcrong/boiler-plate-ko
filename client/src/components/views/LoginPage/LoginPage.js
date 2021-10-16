e42d160af20f87f8468102f4b43655f0b250b50c
kyuyoungoh <oqoq59@gmail.com>
2021-10-16 17:13:55 +0900

    login page

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 function LoginPage() {
+
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+    const onSubmitHandler = (event) => {
+        
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display:'flex', justifyContent:'center', alignItems:'center',
+            width:'100%', height:'100vh'
+        }}>
+            <form style={{display: 'flex', flexDirection: 'column'}} onSubmit={onSubmitHandler}>
+                <label>email</label>
+                <input type= 'email' value={Email} onChange={onEmailHandler} />
+                <label>password</label>
+                <input type= 'password' value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button type='submit'> 
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
